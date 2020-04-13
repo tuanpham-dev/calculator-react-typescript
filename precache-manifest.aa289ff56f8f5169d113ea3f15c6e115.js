7bd6e24a3928b4185bfa629ad1cb14b9e9d20a2a
Tuan Pham <tuanpham.developer@gmail.com>
2020-04-12 20:56:23 -0400

    Updates

---
+++ precache-manifest.aa289ff56f8f5169d113ea3f15c6e115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026ac8b097e04c1f828b28e6c903b837",
+    "url": "/calculator-react-typescript/index.html"
+  },
+  {
+    "revision": "3aa8146d6f8f92538da3",
+    "url": "/calculator-react-typescript/static/css/main.27353e52.chunk.css"
+  },
+  {
+    "revision": "bde90e0cab77eeb0a79f",
+    "url": "/calculator-react-typescript/static/js/2.5a054407.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator-react-typescript/static/js/2.5a054407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa8146d6f8f92538da3",
+    "url": "/calculator-react-typescript/static/js/main.092e2736.chunk.js"
+  },
+  {
+    "revision": "6fe01d20cbec7a6303e6",
+    "url": "/calculator-react-typescript/static/js/runtime-main.c07dd80a.js"
+  }
+]);
