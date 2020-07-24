428884af4d6a9d81b0cd5700c95461b734972446
Tuan Pham <tuanpham.developer@gmail.com>
2020-07-24 09:15:45 -0400

    Add ripple effect to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -62,16 +62,41 @@ export const StyledButton = styled.button<ButtonProps>`
   padding-bottom: 1em;
   transition: background-color 0.15s ease-in-out, opacity 0.15s ease-in-out;
   ${({ color }) => colorToCss(color)}
-  ${({ isLarge }) => isLarge && css`
-    grid-column-end: span 2;
-  `}
+  ${({ isLarge }) =>
+    isLarge &&
+    css`
+      grid-column-end: span 2;
+    `}
+
+  position: relative;
+  overflow: hidden;
+  transform: translate3d(0, 0, 0);
 
   &:focus {
     outline: 0;
   }
 
-  &:active {
-    opacity: 0.9;
+  :after {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    pointer-events: none;
+    background-image: radial-gradient(circle, #fff 10%, transparent 10.01%);
+    background-repeat: no-repeat;
+    background-position: 50%;
+    transform: scale(10, 10);
+    opacity: 0;
+    transition: transform 0.3s, opacity 1s;
+  }
+
+  :active:after {
+    transform: scale(0, 0);
+    opacity: 0.2;
+    transition: 0s;
   }
 `
 
